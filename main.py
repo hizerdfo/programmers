a02277dd33bb5a4845c8e878b8f14b31c7349f85
khy0215 <76670509+khy0215@users.noreply.github.com>
2023-05-02 22:49:30 +0900

    20230502 programmers

--- main.py
+++ main.py
@@ -9,3 +9,45 @@ num2 = input()
 sum = int(num1) + int(num2)
 print("두 수의 합은 %d입니다." % sum)
 
+# 몫 구하기
+
+def solution(num1, num2):
+    answer = num1 // num2
+    return answer
+
+def solution(num1, num2):
+    answer = num1 % num2
+    return answer
+
+
+def solution(num1, num2):
+    answer = int (1000 * (num1 / num2))
+    return answer
+
+#두 분수의 덧셈 , 기약분수 사용
+
+def solution(numer1, denom1, numer2, denom2):
+    # 두 분모가 같아질 때까지 분모를 공통분모로 변환
+    common_denom = denom1 * denom2
+    new_numer1 = numer1 * denom2
+    new_numer2 = numer2 * denom1
+
+    # 분자와 분모를 각각 더한 후, 최대공약수를 구하여 약분
+    result_numer = new_numer1 + new_numer2
+    result_denom = common_denom
+
+    # 최대공약수 구하기
+    for i in range(2, min(result_numer, result_denom) + 1):
+        while result_numer % i == 0 and result_denom % i == 0:
+            result_numer //= i
+            result_denom //= i
+
+    # 결과를 기약분수 형태로 반환
+    return [result_numer, result_denom]
+
+
+def solution(numbers):
+    for i in range(len(numbers)):
+        numbers[i] *= 2;
+    answer =numbers
+    return answer
