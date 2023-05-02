b7412e69e4bc215aca2b24d7fc0daa2ec2524bdb
khy0215 <76670509+khy0215@users.noreply.github.com>
2023-05-02 21:37:32 +0900

    programmers solution

---
+++ main.py
@@ -0,0 +1,11 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+num1 = input()
+num2 = input()
+
+sum = int(num1) + int(num2)
+print("두 수의 합은 %d입니다." % sum)
+
